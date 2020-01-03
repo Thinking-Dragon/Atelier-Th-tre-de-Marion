f7459235e600fae51d4b6db2a5422d9911255028
Clément Gassmann-Prince <cagap8@gmail.com>
2020-01-03 14:10:59 -0500

    Added WIP and interest meter button to "Shop" view

--- solution/atelier-theatre-de-marion/src/views/Shop.vue
+++ solution/atelier-theatre-de-marion/src/views/Shop.vue
@@ -1,3 +1,8 @@
 <template>
-    <div></div>
+    <div>
+        Nous prévoyons ouvrir un magasin où vous pourrez achter des ensembles de marionnettes et de contes.
+        Vous pourrez également commander sur mesure.
+
+        <button class="btn btn-primary">Signifier mon intérêt</button> 
+    </div>
 </template>
