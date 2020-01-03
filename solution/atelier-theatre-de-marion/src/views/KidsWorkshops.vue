375fe74b797eb272a897991ad18524639436b09d
Clément Gassmann-Prince <cagap8@gmail.com>
2020-01-03 01:27:19 -0500

    Included "WorkshopCard" components in "KidsWorkshops" view

--- solution/atelier-theatre-de-marion/src/views/KidsWorkshops.vue
+++ solution/atelier-theatre-de-marion/src/views/KidsWorkshops.vue
@@ -1,3 +1,35 @@
 <template>
-    <div></div>
+    <div class="row">
+        <WorkshopCard
+            class="col-3"
+            title="Parents et tout petits"
+            accent-colour="primary"
+            image=""
+            details="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        />
+        <WorkshopCard
+            class="col-3 offset-1"
+            title="De 3 à 5 ans"
+            accent-colour="warning"
+            image=""
+            details="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        />
+        <WorkshopCard
+            class="col-3 offset-1"
+            title="De 6 à 12 ans"
+            accent-colour="success"
+            image=""
+            details="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        />
+    </div>
 </template>
+
+<script>
+import WorkshopCard from "@/components/WorkshopCard.vue";
+
+export default {
+    components: {
+        WorkshopCard
+    }
+}
+</script>
