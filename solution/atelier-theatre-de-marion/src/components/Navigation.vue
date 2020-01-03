e5fb9d0619c42d7c0c2e739b94f96a1cab727837
Clément Gassmann-Prince <cagap8@gmail.com>
2020-01-02 21:38:28 -0500

    Corrected indentation in "Navigation.vue"

--- solution/atelier-theatre-de-marion/src/components/Navigation.vue
+++ solution/atelier-theatre-de-marion/src/components/Navigation.vue
@@ -1,10 +1,10 @@
 <template>
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/kids-workshops">Ateliers artistiques pour enfants</router-link> |
-      <router-link to="/masterclasses">Cours de formation pour adultes</router-link> |
-      <router-link to="/shop">Boutique</router-link> |
-      <router-link to="/mission">Mission</router-link> |
-      <router-link to="/contact-us">Contactez-nous!</router-link>
+        <router-link to="/">Home</router-link> |
+        <router-link to="/kids-workshops">Ateliers artistiques pour enfants</router-link> |
+        <router-link to="/masterclasses">Cours de formation pour adultes</router-link> |
+        <router-link to="/shop">Boutique</router-link> |
+        <router-link to="/mission">Mission</router-link> |
+        <router-link to="/contact-us">Contactez-nous!</router-link>
     </div>
 </template>
