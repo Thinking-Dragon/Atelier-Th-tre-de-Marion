98105615691f07e2bac848bac209fc964076182c
Clément Gassmann-Prince <cagap8@gmail.com>
2020-01-03 01:26:48 -0500

    Created "WorkshopCard" component

---
+++ solution/atelier-theatre-de-marion/src/components/WorkshopCard.vue
@@ -0,0 +1,35 @@
+<template>
+    <div namespace="workshop-card">
+        <div class="card mb-3" :style="cssProps">
+            <div class="card-header">{{ title }}</div>
+            <div class="card-body">
+                <p class="card-text">{{ details }}</p>
+                <button class="btn btn-primary">Demander des plus d'informations</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        title: String,
+        accentColour: String,
+        image: String,
+        details: String
+    },
+    computed: {
+        cssProps() {
+            return {
+                '--colour': 'var(--' + this.accentColour + ')'
+            }
+        }
+    }
+}
+</script>
+
+<style>
+    [namespace=workshop-card] div.card {
+        border-top: 3px solid var(--colour)
+    }
+</style>
