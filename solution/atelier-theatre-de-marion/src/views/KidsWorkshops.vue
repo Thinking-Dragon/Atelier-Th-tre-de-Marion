42b830baa59ebe3afb0e6ae2909e055169c7104a
Clément Gassmann-Prince <cagap8@gmail.com>
2020-01-04 18:42:20 -0500

    Made WorkshopCard take ~ the whole screen in KidsWorkshops view

--- solution/atelier-theatre-de-marion/src/views/KidsWorkshops.vue
+++ solution/atelier-theatre-de-marion/src/views/KidsWorkshops.vue
@@ -1,21 +1,21 @@
 <template>
     <div class="row">
         <WorkshopCard
-            class="col-3"
+            class="col-4"
             title="Parents et tout petits"
             accent-colour="primary"
             image="logo.png"
             details="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
         />
         <WorkshopCard
-            class="col-3 offset-1"
+            class="col-4"
             title="De 3 à 5 ans"
             accent-colour="warning"
             image="brand/logo-brown.svg"
             details="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
         />
         <WorkshopCard
-            class="col-3 offset-1"
+            class="col-4"
             title="De 6 à 12 ans"
             accent-colour="success"
             image="logo.png"
