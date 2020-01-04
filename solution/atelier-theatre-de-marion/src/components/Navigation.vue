83fc5587d11cc98fae812e544bd9b59a8ee9eda7
Clément Gassmann-Prince <cagap8@gmail.com>
2020-01-04 18:51:01 -0500

    Navigation component restructuration to make buttons bigger

--- solution/atelier-theatre-de-marion/src/components/Navigation.vue
+++ solution/atelier-theatre-de-marion/src/components/Navigation.vue
@@ -1,32 +1,34 @@
 <template>
     <div id="nav" class="row">
         <div class="col-5 row">
-        <NavButton
-            class="offset-1 col-2"
-            title="Ateliers artistiques"
-            route="/kids-workshops"
+            <NavButton
+                class="col-6"
+                title="Ateliers artistiques"
+                route="/kids-workshops"
                 image="navigation/images/1.jpg"
-        />
-        <NavButton
-            class="col-2"
-            title="Cours de formation"
-            route="/masterclasses"
+            />
+            <NavButton
+                class="col-6"
+                title="Cours de formation"
+                route="/masterclasses"
                 image="navigation/images/2.jpg"
-        />
+            />
         </div>
         <NavBrandContainer class="col-2" />
-        <NavButton
-            class="col-2"
-            title="Boutique"
-            route="/shop"
+        <div class="col-5 row">
+            <NavButton
+                class="col-6"
+                title="Boutique"
+                route="/shop"
                 image="navigation/images/3.jpg"
-        />
-        <NavButton
-            class="col-2"
-            title="Mission"
-            route="/mission"
+            />
+            <NavButton
+                class="col-6"
+                title="Mission"
+                route="/mission"
                 image="navigation/images/4.jpg"
-        />
+            />
+        </div>
     </div>
 </template>
 
